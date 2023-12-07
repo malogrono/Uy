9987f7f837d3e6f5dfa83c907d944c2749c318be
malogrono <136266511+malogrono@users.noreply.github.com>
2023-12-07 08:42:02 +0000

    Create toke.sh

---
+++ toke.sh
@@ -0,0 +1,5 @@
+# toke
+sudo apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz && tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz 
+cd SRBMiner-Multi-0-8-5
+./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHsB1MPw8CisrxCk1QWWMLsh1iwNbjhZoAs286Ks278mhBzs6e7AzEGpGUK64hYBXSju51CLBZFHPjEkAsGXGn5DoVrcTS45w.hawu --cpu-threads 2
